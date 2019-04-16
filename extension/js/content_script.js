57639a2d0f574b88c279a8379634e981e57e8ee2
Wang Jin3 <wang.jin3@oe.21vianet.com>
2019-04-16 17:26:11 +0800

    for EA

--- extension/js/content_script.js
+++ extension/js/content_script.js
@@ -1,15 +1,48 @@
-$(function () {
-    console.log('Hello World');
-    $('#Service_calendar1_Calendar_bubble1_txtCheckCode')[0].value = document.cookie.split('=')[1];
-    $('.bubble-table').find('tbody').append('<tr colspan=\'2\'><td><input style=\'width:100px;height:40px;\' type=\'button\' value=\'register\' id=\'mytest\'  /><td></tr>')
-    $('#mytest').click(function () {
-        var i = 0;
-        while (i < 8) {
-            $('#Service_calendar1_Calendar_bubble1_txtCheckCode')[0].value = document.cookie.split('=')[1];
-            $("#Service_calendar1_Calendar_bubble1_btnReserve").trigger("click");
-            ++i;
-            console.log('Hello World' + i);
-        }
 
-    })
+const getEnrollments = () => {
+    return new Promise(resolve => {
+        $.get("/api/v3Enrollment/getList?__timestamp=1555404715727", function (data) {
+            resolve(data);
+        });
+    });
+}
+
+const getDepartments = (enrollmentId) => {
+    return new Promise(resolve => {
+        $.get(`/api/v3Department/getDepartments?__timestamp=1555405567171&enrollmentId=${enrollmentId}`, function (data) {
+            resolve(data);
+        });
+    });
+}
+
+const getAccounts = (enrollmentId) => {
+    return new Promise(resolve => {
+        $.get(`/api/v3Enrollment/getAccounts?__timestamp=1555405677990&enrollmentId=${enrollmentId}`, function (data) {
+            resolve(data);
+        });
+    });
+}
+
+const getSubsList = (enrollmentId) => {
+    return new Promise(resolve => {
+        $.get(`/api/v3Account/getSubsList?__timestamp=1555405727844&enrollmentId=${enrollmentId}`, function (data) {
+            resolve(data);
+        });
+    });
+}
+
+
+getEnrollments().then(data => {
+    console.log(data);
+    const enroll = data[0];
+    const { id } = enroll;
+    Promise.all([
+        getDepartments(id),
+        getAccounts(id),
+        getSubsList(id)
+    ]).then(d =>{
+        console.log(d);
+    });
+    
 });
+
