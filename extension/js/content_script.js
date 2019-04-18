beaf6d87460ba6ef95853be2f49674bc68a9665f
Wang Jin3 <wang.jin3@oe.21vianet.com>
2019-04-18 17:13:28 +0800

    updates

--- extension/js/content_script.js
+++ extension/js/content_script.js
@@ -1,32 +1,58 @@
 
 const getEnrollments = () => {
+    const timestamp = (new Date()).getTime();
     return new Promise(resolve => {
-        $.get("/api/v3Enrollment/getList?__timestamp=1555404715727", function (data) {
+        $.get(`/api/v3Enrollment/getList?__timestamp=${timestamp}`, function (data) {
             resolve(data);
         });
     });
 }
 
 const getDepartments = (enrollmentId) => {
+    const timestamp = (new Date()).getTime();
     return new Promise(resolve => {
-        $.get(`/api/v3Department/getDepartments?__timestamp=1555405567171&enrollmentId=${enrollmentId}`, function (data) {
+        $.get(`/api/v3Department/getDepartments?__timestamp=${timestamp}&enrollmentId=${enrollmentId}`, function (data) {
             resolve(data);
         });
     });
 }
 
 const getAccounts = (enrollmentId) => {
+    const timestamp = (new Date()).getTime();
     return new Promise(resolve => {
-        $.get(`/api/v3Enrollment/getAccounts?__timestamp=1555405677990&enrollmentId=${enrollmentId}`, function (data) {
+        $.get(`/api/v3Enrollment/getAccounts?__timestamp=${timestamp}&enrollmentId=${enrollmentId}`, function (data) {
             resolve(data);
         });
     });
 }
 
 const getSubsList = (enrollmentId) => {
+    const timestamp = (new Date()).getTime();
     return new Promise(resolve => {
-        $.get(`/api/v3Account/getSubsList?__timestamp=1555405727844&enrollmentId=${enrollmentId}`, function (data) {
+        $.get(`/api/v3Account/getSubsList?__timestamp=${timestamp}&enrollmentId=${enrollmentId}`, function (data) {
             resolve(data);
+        });
+    });
+}
+
+
+
+const putData = (input) => {
+    var enc = new TextEncoder(); // always utf-8
+    // const array = enc.encode();
+    // const timestamp = (new Date()).getTime();
+    return new Promise(resolve => {
+        $.ajax({
+            url: 'http://localhost:56621/crawler/cache', // your api url
+            contentType: 'application/json; charset=utf-8',
+            
+            // jQuery < 1.9.0 -> use type
+            // jQuery >= 1.9.0 -> use method
+            method: 'PUT', // method is any HTTP method
+            data: JSON.stringify(input), // data as js object
+            success: function(data) {
+                resolve(data);
+            }
         });
     });
 }
@@ -41,6 +67,12 @@ getEnrollments().then(data => {
         getAccounts(id),
         getSubsList(id)
     ]).then(d =>{
+        putData({
+            enrollmentEntity: enroll,
+            departmentResult: d[0],
+            acctResult: d[1],
+            subResult: d[2]
+        });
         console.log(d);
     });
     
